9c63d5996f18333bd27672a7fbcbf2f6629aef34
Kimia K. Ahrabi <kimia96@gmail.com>
2019-12-05 21:10:48 -0500

    add cube behavior

---
+++ Assets/CubeBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, 100 * Time.deltaTime);
+    }
+}
